50230529d1ab108388e88d99b1fe7fb39b22779c
anand <anandkumarchatra969@gmail.com>
2025-08-28 01:47:30 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>
+        &copy; 2023 <b>Aniruddha Patil</b>, All rights reserved.
+      </p>
+    </div>
+  );
+}
